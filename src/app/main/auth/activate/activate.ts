0842d6e36168f7b9e929483958a3121014baa030
MartinBock1 <martinbock@arcor.de>
2025-08-13 12:09:05 +0200

    Add activate component with HTML template, SCSS styles, and unit tests

---
+++ src/app/main/auth/activate/activate.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activate',
+  imports: [],
+  templateUrl: './activate.html',
+  styleUrl: './activate.scss'
+})
+export class Activate {
+
+}
