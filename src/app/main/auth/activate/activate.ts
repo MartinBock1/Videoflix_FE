05ee05a693fb6d73053c29717d4cba59c5c3e7c6
MartinBock1 <martinbock@arcor.de>
2025-08-13 13:11:58 +0200

    Enhance authentication UI by adding headers and footers to activation, confirmation, forgot password, login, and registration components; update header component to accept dynamic action link and text.

--- src/app/main/auth/activate/activate.ts
+++ src/app/main/auth/activate/activate.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Header } from '../../../shared/header/header';
+import { Footer } from '../../../shared/footer/footer';
 
 @Component({
   selector: 'app-activate',
-  imports: [],
+  imports: [Header, Footer],
   templateUrl: './activate.html',
   styleUrl: './activate.scss'
 })
--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -8,5 +8,6 @@ import { RouterModule } from '@angular/router';
   styleUrl: './header.scss'
 })
 export class Header {
-
+  @Input() actionLink: string = '/auth/login';
+  @Input() actionText: string = 'Log in';
 }
