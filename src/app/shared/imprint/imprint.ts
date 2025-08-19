960f93ef78d65ad1dd9bcf57707e7fe5b69a5ce6
MartinBock1 <martinbock@arcor.de>
2025-08-19 12:13:13 +0200

    Implement imprint page: add header and footer components, structure HTML content with contact information, and create associated styles for layout and readability

--- src/app/shared/imprint/imprint.ts
+++ src/app/shared/imprint/imprint.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { Header } from '../../shared/header/header';
+import { Footer } from '../../shared/footer/footer';
 
 @Component({
   selector: 'app-imprint',
-  imports: [],
+  imports: [Header, Footer],
   templateUrl: './imprint.html',
-  styleUrl: './imprint.scss'
+  styleUrl: './imprint.scss',
 })
-export class Imprint {
-
-}
+export class Imprint {}
